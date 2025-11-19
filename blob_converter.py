e7aa8d3a58ee08675fc3257bebb308817a6ff85a
Mohamad Alkhabaz <mohamad@ucsb.edu>
2025-11-18 21:05:42 -0800

    Add files via upload
    Use this script to convert model to a .blob which can be used in object_detection.py

---
+++ blob_converter.py
@@ -0,0 +1,7 @@
+import blobconverter
+
+blob_path = blobconverter.from_zoo(
+    name="/home/mohamad/depthai-python/depthai-model-zoo/models/yolov8n_coco_640x352",
+    shaves=6,
+    zoo_type="depthai"
+)
